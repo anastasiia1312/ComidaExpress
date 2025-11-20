9ee4ab199311e9366e86fb048d16f8b06771dec1
anastasiia1312 <anastasiia.suslova@davinci.edu.ar>
2025-11-20 20:31:32 -0300

    Commit3

--- app/__init__.py
+++ app/__init__.py
@@ -17,11 +17,15 @@ def create_app():
     db.init_app(app)
     login_manager.init_app(app)
 
+    with app.app_context():
+        db.create_all()
+
     # register blueprints
     from app.auth.routes import main
     app.register_blueprint(main)
 
     return app
+
 
 
 
